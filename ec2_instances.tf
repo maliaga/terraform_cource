3c4f1d9390b6d2a5debf57f934371bbc3724b799
Mario Aliaga <mario@aliaga.dev>
2022-01-26 08:49:18 -0300

    Add interpolation on instances name

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server1" {
     ami = var.ami_id
     instance_type = "t2.micro"
-    count = 1
+    count = 3
     subnet_id = aws_subnet.subnet1.id
     vpc_security_group_ids = [aws_security_group.sg1.id, aws_security_group.sg2.id]
     private_ip = "10.0.10.10"
@@ -9,7 +9,7 @@ resource "aws_instance" "server1" {
     user_data = file("userdata.sh")
 
     tags = {
-        Name = "server1"
+        Name = "server-${count.index}"
         Owner = "Mario"
         Env = "Dev"
     }
